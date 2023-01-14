0847036884c85a57b01409e9c93eae72487c65c6
tamago0224 <bigtreesasuke200733@gmail.com>
2023-01-14 11:08:37 +0900

    added user id column in todos table

--- sql/01-initial-schema.sql
+++ sql/01-initial-schema.sql
@@ -1,11 +1,13 @@
-CREATE TABLE todos (
-    id  integer not null primary key auto_increment,
-    title VARCHAR(255) NOT NULL,
-    description TEXT
-);
-
 CREATE TABLE users (
     id integer not null primary key auto_increment,
     name VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
-)
+);
+
+CREATE TABLE todos (
+    id  integer not null primary key auto_increment,
+    user_id integer not null,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    foreign key (user_id) references users (id) on delete cascade
+);
